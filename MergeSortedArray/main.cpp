d64f5dcaa65e2a808b470ee2d60a9be2d5fdc7ff
Lyphard <137520956+lyphardlu@users.noreply.github.com>
2023-07-28 15:08:39 +0800

    Update main.cpp

--- MergeSortedArray/main.cpp
+++ MergeSortedArray/main.cpp
@@ -1,1 +1,27 @@
-test
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        /* approach 1 
+        int i = 0;
+        for(i = 0; i < n; i++) {
+            nums1[m+i] = nums2[i];
+        }
+        sort(nums1.begin(), nums1.end()); */
+      
+        // approach 2
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+        while(i>=0 && j >=0) {
+            if(nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        while(j>=0)
+        {
+            nums1[k--] = nums2[j--];
+        }
+    }
+};
